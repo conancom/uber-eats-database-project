fa290b18e7e69335abee17f8fbdff89e3a09786a
conancom <cnoanplays@gmail.com>
2021-11-19 15:05:16 +0700

    Finished edit menu item page

--- Restaurant/Menu-Restaurant-Edit.php
+++ Restaurant/Menu-Restaurant-Edit.php
@@ -24,6 +24,53 @@ if (isset($_SESSION['id-restaurant'])) {
         }
     }
 }
+
+
+if (isset($_SESSION['id-restaurant']) and isset($_GET['id'])) {
+
+    $query1 = "SELECT * FROM `menuitem` WHERE `MenuItemID` = '$menuitemId '";
+    // print($query); 
+    $result1 = $mysqli->query($query1);
+    if (!$result1) {
+        echo $mysqli->error;
+    } else {
+        if (mysqli_num_rows($result1) > 0) {
+            $data1 = $result1->fetch_array();
+        }
+    }
+}
+
+
+if (isset($_SESSION['id-restaurant']) and isset($_POST['submit-add'])) {
+    $restaurantid = $_SESSION['id-restaurant'];
+    $name = $_POST['name'];
+    $type = $_POST['type'];
+    $price = $_POST['price'];
+    $calories = $_POST['calories'];
+    $ingredient = $_POST['ingredients'];
+    $portion = $_POST['portion'];
+    $FoodDescription = $_POST['FoodDescription'];
+    $query = "UPDATE `menuitem` SET  `FoodName` = '$name', `FoodType` = '$type', `Price` = '$price', `Calories` = '$calories', `Ingredient` = '$ingredient', `Portion` = '$portion', `FoodDescription`= '$FoodDescription' 
+    WHERE `MenuItemID` = '$menuitemId'";
+    // print($query); 
+    $result = $mysqli->query($query);
+    if (!$result) {
+        echo $mysqli->error;
+    } else {
+
+
+        if (file_exists('foodimg/' . $menuitemId . '.jpg')) {
+            unlink('foodimg/' . $menuitemId . '.jpg');
+        }
+
+
+        move_uploaded_file($_FILES["my_file"]["tmp_name"], 'foodimg/' . $menuitemId . '.jpg');
+        header("Location: Menu-Restaurant.php");
+
+
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -86,54 +133,52 @@ if (isset($_SESSION['id-restaurant'])) {
 
     <div class="AddMenuFormContainer">
         <div class="div_content" class="form">
-            <form name="add-menuitem" action="" method="post">
-                <div class="text_wrapper" style="margin-top:25;">
-                    <label class="text_itemdi" style="padding-right: 35px;">Item ID</label>
-                    <input type="text" name="itemid" class="text_field" placeholder=" Item ID"><br>
-                </div>
+            <form name="add-menuitem" action="" method="post" enctype="multipart/form-data">>
 
                 <div class="text_wrapper">
                     <label class="text_name" style="padding-right: 47px"> Name </label>
-                    <input type="text" name="name" class="text_field" placeholder=" Name"><br>
+                    <input type="text" name="name" class="text_field" placeholder=" Name" <?php echo 'value= "' . $data1['FoodName'] . '"'  ?>><br>
                 </div>
 
                 <div class="text_wrapper">
                     <label class="text_type" style="padding-right: 53px"> Type </label>
-                    <input type="text" name="type" class="text_field" placeholder=" Type"><br>
+                    <input type="text" name="type" class="text_field" placeholder=" Type" <?php echo 'value= "' . $data1['FoodType'] . '"'  ?>><br>
                 </div>
 
                 <div class="text_wrapper">
                     <label class="text_price" style="padding-right: 52px"> Price </label>
-                    <input type="text" name="price" class="text_field" placeholder=" Price"><br>
+                    <input type="text" name="price" class="text_field" placeholder=" Price" <?php echo 'value= "' . $data1['Price'] . '"'  ?>><br>
                 </div>
 
                 <div class="text_wrapper">
                     <label class="text_calories" style="padding-right: 31px"> Calories </label>
-                    <input type="text" name="calories" class="text_field" placeholder=" Calories"><br>
+                    <input type="text" name="calories" class="text_field" placeholder=" Calories" <?php echo 'value= "' . $data1['Calories'] . '"'  ?>><br>
                 </div>
 
                 <div class="text_wrapper">
                     <label class="text_ingredients" style="padding-right: 13px"> Ingredients </label>
-                    <input type="text" name="ingredients" class="text_field" placeholder=" Ingredients"><br>
+                    <input type="text" name="ingredients" class="text_field" placeholder=" Ingredients" <?php echo 'value= "' . $data1['Ingredient'] . '"'  ?>><br>
                 </div>
 
-                <div class="text_wrapper">
-                    <label class="text_amountsold"> Amount Sold </label>
-                    <input type="text" name="amountsold" class="text_field" placeholder=" Amount Sold"><br>
-                </div>
 
                 <div class="text_wrapper">
                     <label class="text_portion" style="padding-right: 37px"> Portion </label>
-                    <input type="text" name="portion" class="text_field" placeholder=" Portion"><br>
+                    <input type="text" name="portion" class="text_field" placeholder=" Portion" <?php echo 'value= "' . $data1['Portion'] . '"'  ?>><br>
                 </div>
 
                 <div class="text_wrapper">
                     <label class="text_portion"> Description </label> <br>
-                    <textarea name="w3review" rows="4" cols="50" class="text_area" placeholder=" Description" style="margin-left: 155px;"></textarea>
+                    <textarea name="FoodDescription" rows="4" cols="50" class="text_area" placeholder=" Description" style="margin-left: 155px;"> <?php echo $data1['FoodDescription'] ?> </textarea>
                 </div>
+                <br><br>
+
+                Select Image to upload:
+                <input type="file" name="my_file" />
+
+                <br><br>
 
                 <div class="button">
-                    <button type="submit" name="submit" value="Add" class="AddButton">Confirm</button>
+                    <input type="submit" name="submit-add" value="Add" class="AddButton"></button>
                 </div>
             </form>
 
@@ -145,11 +190,10 @@ if (isset($_SESSION['id-restaurant'])) {
     </div>
 
     <div class="edittext">
-        Edit
+
     </div>
     <div class="buttonbox">
-        <button class="AddItemButton"> Add Item </button>
-        <button class="DeleteItemButton"> Delete Item </button>
+
     </div>
     </div>
 
